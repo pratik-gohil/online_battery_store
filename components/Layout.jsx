a5d995d95f1fa29352c8c4f7e7a4e8f4a7f5598e
Pratik Gohil <pr.tikgohil@gmail.com>
2023-03-26 14:49:51 +0530

    fixed: mobile responsiveness

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useStateContext } from "../context/StateContext";
 
 const Layout = ({ children }) => {
+  const { showCart } = useStateContext();
+
+  useEffect(() => {
+    showCart
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [showCart]);
+
   return (
-    <div className="layout">
+    <div className={showCart || "layout"}>
       <Head>
         <title>Shivsai Batteries</title>
       </Head>
