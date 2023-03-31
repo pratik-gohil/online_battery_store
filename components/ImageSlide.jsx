df74066b49a551978f9b151e1351084c00f3d325
Pratik Gohil <pr.tikgohil@gmail.com>
2023-03-31 20:34:42 +0530

    removed dev changes

--- components/ImageSlide.jsx
+++ components/ImageSlide.jsx
@@ -3,21 +3,21 @@ import React, { useEffect, useRef, useState } from "react";
 function ImageSlide({ images }) {
   const slideRef = useRef();
 
-  // useEffect(() => {
-  //   let index = 0;
-  //   const slideIntervaal = setInterval(() => {
-  //     if (slideRef && slideRef.current && slideRef.current.clientWidth) {
-  //       const slideWidth = slideRef.current.clientWidth;
-  //       slideRef.current.scrollTo({
-  //         left: slideWidth * index,
-  //         behavior: index ? "smooth" : "instant",
-  //       });
-  //       index < images.length ? index++ : (index = 0);
-  //     }
-  //   }, 3000);
+  useEffect(() => {
+    let index = 0;
+    const slideIntervaal = setInterval(() => {
+      if (slideRef && slideRef.current && slideRef.current.clientWidth) {
+        const slideWidth = slideRef.current.clientWidth;
+        slideRef.current.scrollTo({
+          left: slideWidth * index,
+          behavior: index ? "smooth" : "instant",
+        });
+        index < images.length ? index++ : (index = 0);
+      }
+    }, 3000);
 
-  //   return () => clearInterval(slideIntervaal);
-  // }, []);
+    return () => clearInterval(slideIntervaal);
+  }, []);
 
   return (
     <div className="image-slide" ref={slideRef}>
