07b6a22e03996d997f5a14ed87fcff345b0ea0ff
Pratik Gohil <pr.tikgohil@gmail.com>
2023-04-24 00:02:34 +0530

    added: car-batteries page

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -1,40 +1,51 @@
 export default {
-  name: 'product',
-  title: 'Product',
-  type: 'document',
+  name: "product",
+  title: "Product",
+  type: "document",
   fields: [
     {
-      name: 'image',
-      title: 'Image',
-      type: 'array',
-      of: [{ type: 'image' }],
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
       options: {
         hotspot: true,
-      }
+      },
     },
-    { 
-      name: 'name',
-      title: 'Name',
-      type: 'string',
+    {
+      name: "compatible_models",
+      title: "Compatible Models",
+      type: "array",
+      of: [{ type: "string" }],
     },
-    { 
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+    {
+      name: "brand",
+      title: "Brand",
+      type: "string",
+    },
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'name',
+        source: "name",
         maxLength: 90,
-      }
+      },
     },
-    { 
-      name: 'price',
-      title: 'Price',
-      type: 'number',
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
     },
-    { 
-      name: 'details',
-      title: 'Details',
-      type: 'string',
-    }
-  ]
-}
+    {
+      name: "details",
+      title: "Details",
+      type: "string",
+    },
+  ],
+};
