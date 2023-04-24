74f29f7b017ec88e239594113f070fcdf6d35d63
Pratik Gohil <pr.tikgohil@gmail.com>
2023-04-24 15:39:22 +0530

    fixed: webhook construct event

--- pages/api/stripe-webhook.js
+++ pages/api/stripe-webhook.js
@@ -1,10 +1,9 @@
 import { buffer } from "micro";
+import Cors from "micro-cors";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../fireabase.config";
-
-import Cors from "micro-cors";
 
 const cors = Cors({
   allowMethods: ["POST", "HEAD"],
@@ -25,7 +24,11 @@ async function webhookHandler(req, res) {
     let event;
 
     try {
-      event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
+      event = stripe.webhooks.constructEvent(
+        buf.toString(),
+        sig,
+        webhookSecret
+      );
     } catch (err) {
       console.error("⚠️ Webhook signature verification failed.", err.message);
       res.status(400).send(`Webhook Error: ${err.message}`);
